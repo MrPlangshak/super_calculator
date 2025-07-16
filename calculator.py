26172fa7e0e6b74d70bc776a283bde5540ccba3a
Mrplangshak <dankyesplang@gmail.com>
2025-07-16 12:59:49 +0000

    first commit

---
+++ calculator.py
@@ -0,0 +1,38 @@
+#A simple calculator app
+print ("""What our app does
+1. Addition
+2. subtraction
+3. Multiplication
+4. Division
+5. Exponential""")
+"""
+print ("Enter two numbers to add: ")
+first_number = float( input("First Number:"))
+second_number = float(input("Second Number:"))
+sum = first_number + second_number
+print (f"{first_number} + {second_number} is {sum}")
+
+print ("Enter two numbers to subtract: ")
+first_number = float( input("First Number:"))
+second_number = float(input("Second Number:"))
+sub = first_number - second_number
+print (f"{first_number} - {second_number} is {sub}")
+ 
+print ("Enter two numbers to multiply: ")
+first_number = float( input("First Number:"))
+second_number = float(input("Second Number:"))
+mul = first_number * second_number
+print (f"{first_number} * {second_number} is {mul}")
+
+
+print ("Enter two numbers to divide: ")
+first_number = float( input("First Number:"))
+second_number = float(input("Second Number:"))
+div = first_number / second_number
+print (f"{first_number} / {second_number} is {div}")
+"""
+
+print ("Enter a number to get exponential: ")
+number = float( input("Number:"))
+exp = number **2
+print (f"first_number exponential is {exp}")
