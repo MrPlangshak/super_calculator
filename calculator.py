33414276265cb85f1ea1574338eb22d5ba8590c4
Mrplangshak <dankyesplang@gmail.com>
2025-07-16 13:42:48 +0000

    to 2f

--- calculator.py
+++ calculator.py
@@ -9,31 +9,31 @@ print ("""What our app does
 print ("Enter two numbers to add: ")
 first_number = float( input("First Number:"))
 second_number = float(input("Second Number:"))
-sum = first_number + second_number
-print (f"{first_number} + {second_number} is {sum}0")
+sum = ((first_number + second_number))
+print (f"{first_number} + {second_number} is {sum:.2f}")
 
 print ("Enter two numbers to subtract: ")
 first_number = float( input("First Number:"))
 second_number = float(input("Second Number:"))
 sub = first_number - second_number
-print (f"{first_number} - {second_number} is {sub}0")
+print (f"{first_number} - {second_number} is {sub:.2f}")
  
 print ("Enter two numbers to multiply: ")
 first_number = float( input("First Number:"))
 second_number = float(input("Second Number:"))
 mul = first_number * second_number
-print (f"{first_number} * {second_number} is {mul}0")
+print (f"{first_number} * {second_number} is {mul:.2f}")
 
 
 print ("Enter two numbers to divide: ")
 first_number = float( input("First Number:"))
 second_number = float(input("Second Number:"))
 div = first_number / second_number
-print (f"{first_number} / {second_number} is {div}0")
+print (f"{first_number} / {second_number} is {div:.2f}")
 
 
 print ("Enter two numbers to get exponential: ")
 first_number = float( input("First Number:"))
 second_number = float(input("Second Number:"))
 exp = first_number ** second_number
-print (f"{first_number} ** {second_number} is {exp}0")
+print (f"{first_number} ** {second_number} is {exp:.2f}")
